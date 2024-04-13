831c41498fd1ea3e61766a9e46dce68c09290710
M Lintang Ashshofa W <ashshofalintang@gmail.com>
2024-04-13 20:59:58 +0700

    update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rkh-website/';
+$config['base_url'] = 'http://108.136.248.218/rkh23-main/';
 
 /*
 |--------------------------------------------------------------------------
